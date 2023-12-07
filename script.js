66720167f640bb3ca6bc73f532640acd81879790
moromoke01 <56760496+moromoke01@users.noreply.github.com>
2023-12-07 07:26:20 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const myImage = document.getElementById('img1');
+document.addEventListener('DOMContentLoaded', function() {
 
-myImage.addEventListener("click", (event) => {
-    event.target.style.background = "red";
-    event.target.style.color ="white";
+    var paragraphs = document.querySelectorAll('.imagename');
 
+    function handleParagraphClick(event) {
+        event.target.style.backgroundColor = "red";
+        event.target.style.color = "white";
+    }
+
+    paragraphs.forEach(function(paragraph) {
+        paragraph.addEventListener("click", handleParagraphClick);
+    });
 });
